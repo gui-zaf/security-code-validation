4d09e006fd4f87d6c2b735ce97327c6e5ee23256
Guilherme <guigacamposferraz@gmail.com>
2023-12-15 23:27:12 -0300

    add failure shake animation

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 function generateCode() {
     let code = "";
-    let characters = "0123456789";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (let i = 0; i < 5; i++) {
         code += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     document.getElementById("codeDisplay").textContent = code;
     document.getElementById("codeInput").value = "";
     document.getElementById("codeInput").style.color = "#555";
+    document.getElementById("codeInput").classList.remove("failure");
 }
 
 function checkCode() {
     let generatedCode = document.getElementById("codeDisplay").textContent;
     let enteredCode = document.getElementById("codeInput").value;
+    let codeInput = document.getElementById("codeInput");
 
     if (enteredCode === "") {
-        document.getElementById("codeInput").style.color = "#555";
+        codeInput.style.color = "#555";
         generateCode();
     }
 
     if (enteredCode === generatedCode) {
-        document.getElementById("codeInput").style.color = "#65b360";
+        codeInput.style.color = "#65b360";
+        codeInput.classList.remove("failure");
     } else {
-        document.getElementById("codeInput").style.color = "#eb4646";
+        codeInput.style.color = "#eb4646";
+        codeInput.classList.add("failure");
     }
 }
